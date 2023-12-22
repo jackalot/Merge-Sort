8ddf1780709b8ecdc7764d007392e6937de59806
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-22 18:39:51 -0500

    added a comment that describes the algorithem
    added a comment that describes the algorithm, step by step using our example.

--- src/Start.java
+++ src/Start.java
@@ -2,6 +2,18 @@
 public class Start {
 	public static void main(String[] args) {
 		final int[] NUM_ARRAY = {5,3,1,2,4,6,8,7};
-		
+		/*65318724
+		 * SUBDIVIDE
+		 *6531-8724
+		 *SUBDIVIDE UNTIL
+		 *ITS A SUBARRAY OF 2
+		 *65-31-87-24
+		 *SWAP THEM
+		 *56-13-78-24
+		 *ORGANIZE
+		 *1356-2478
+		 *ORGANIZE AGAIN
+		 *56137824
+		 * */
 	}
 }
