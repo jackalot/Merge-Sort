cf5eaa1a066bdfa6b08a4d47cec5fa4a55cfdfc1
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-23 16:59:42 -0500

    Removed a chunk of the subdivide plan
    removed the chunk of the subdivide plan we did already, notably the even side.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,23 +3,11 @@ public class MergeSort {
 /*create a method named subdivide(int[] originalArray)
  * {
  *  find the length of the array
- *  if the length is even {
- *  	indexOne which starts at 0
- 		indexTwo which starts at 1
- * 		for the length of the array {
- * 			have two variables,
- *  		 if indexTwo is less than or
- *  		 equal to the length of the
- *  		 array, stop
- *  		 Create an array list item,
- *  		 array items at indexOne and indexTwo
- *  		 add two to both indexOne and indexTwo
- * 		} 
- * } if the length of the array is odd {
+ *  if the length of the array is odd {
  * 		indexOne starts at 0 
  * 		indexTwo which starts at 1
  * 		for the length of the array {
- * 			if index
+ * 			
  * }
  * */
 	public ArrayList<Integer[]> Subdivide(int[] originalArray)
