221a04ef71e73337eedb036be5e8e2aaca860498
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-22 21:21:18 -0500

    Added a reference to subdivide method and array
    added to the new subdivided method, changed our old array to "EVEN_LENGTH_ARRAY". Created "Odd_LENGTH_ARRAY" as a second test.

--- src/Start.java
+++ src/Start.java
@@ -1,7 +1,11 @@
 
 public class Start {
 	public static void main(String[] args) {
-		final int[] NUM_ARRAY = {5,3,1,2,4,6,8,7};
+		
+		final int[] EVEN_LENGTH_ARRAY = {5,3,1,2,4,6,8,7};
+		final int[] ODD_LENGTH_ARRAY = {5,3,1,2,4,6,8,7,0};
+		MergeSort sort = new MergeSort();
+		sort.Subdivide(NUM_ARRAY);;
 		/*65318724
 		 * SUBDIVIDE
 		 *6531-8724
