89c130c2b5d2608cd61b37f926fd3c2dc8d601de
jackalot <68657425+jackalot@users.noreply.github.com>
2024-01-04 11:32:06 -0500

    removed the portion of our plan thats been done.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -119,8 +119,7 @@ public int[][] JoinArrays(int[][] Subdivisions) {
 		 * [[1,2,3,5],[4,6,7,8]]
 		 * [1,2,3,4,5,6,7,8]
 		 * Heres what we need to do in this method:
-		 *3.  Now we know whats in the first half, lets get the arrayList in order. Lets
-		 * create a method named SortHalfArray();
+		 *4.
 		 *
 		 *
 		 * */
